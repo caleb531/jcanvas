2523fe15dd95076d82bfbc2da15e15f4f7d6770f
Zac Gross <zacgross@gmail.com>
2012-07-14 19:47:08 -0400

    Bug fix for loop in removeLayerGroup function.

--- jcanvas.js
+++ jcanvas.js
@@ -1238,7 +1238,7 @@ $.fn.removeLayerGroup = function(name) {
 		layers = $($elems[e]).getLayers();
 	
 		// Loop through layers array for each element
-		for (l=0; l<layers.length; l+=1) {
+		for(l = layers.length -1; l > 0; l--) {
 			// Remove layer if group name matches
 			if (layers[l].group === name) {
 				layers.splice(l, 1);
