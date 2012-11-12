d472a6054486e0d6a96094d3d211dca05e5f9b3e
Dave K. Smith <dave.k.smith@gmail.com>
2012-11-12 11:33:12 -0600

    DKS - minor reorganization, preparing for rename

--- tests/unit/jcanvas-layers.js
+++ tests/unit/jcanvas-layers.js
@@ -1,5 +1,4 @@
-module("jCanvas Layers");
-
+/* Internal helpers */
 function AtomicCanvas() {
 	// modeled after QUnit's "Keeping Tests Atomic" cookbook guidance:
 	//    http://qunitjs.com/cookbook/#keeping_tests_atomic
@@ -7,6 +6,9 @@ function AtomicCanvas() {
 	this.fixture.append("<canvas id='atomic-canvas' width='50' height='50'></canvas>")
 	this.canvas = document.getElementById('atomic-canvas')
 }
+
+/* jCanvas tests */
+module("jCanvas Layers");
 
 test("addLayer, method drawRect", function () {
 	var ac = new AtomicCanvas()
