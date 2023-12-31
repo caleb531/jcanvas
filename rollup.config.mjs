9715f47e163f9e0dcc26521a2a296d35f59e3c4e
Caleb Evans <caleb@calebevans.me>
2023-12-30 17:01:52 -0800

    Fix indentation in Rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,27 +6,27 @@ import terser from '@rollup/plugin-terser';
 const inputPaths = globSync('src/*.js');
 
 export default inputPaths.map((inputPath) => {
-	const inputFilenameWithoutExtension = path.basename(inputPath, '.js');
-	return {
+  const inputFilenameWithoutExtension = path.basename(inputPath, '.js');
+  return {
     input: [inputPath],
     external: ['jquery'],
     output: [
       {
         file: `dist/umd/${inputFilenameWithoutExtension}.min.js`,
         format: 'umd',
-				name: 'jCanvas',
+        name: 'jCanvas',
         sourcemap: true,
-				globals: {
-					jquery: '$'
-				}
+        globals: {
+          jquery: '$'
+        }
       },
       {
         file: `dist/esm/${inputFilenameWithoutExtension}.min.js`,
         format: 'esm',
         sourcemap: true,
-				globals: {
-					jquery: '$'
-				}
+        globals: {
+          jquery: '$'
+        }
       }
     ],
     plugins: [commonjs(), terser()]
