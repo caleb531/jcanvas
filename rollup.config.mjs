b10fffbed5d4663e91a470aaba20764992ff8373
Caleb Evans <caleb@calebevans.me>
2024-05-23 19:58:22 -0700

    Convert spaces to tabs in Rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,34 +1,34 @@
-import path from 'node:path';
-import { globSync } from 'glob';
-import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import { globSync } from "glob";
+import path from "node:path";
 
-const inputPaths = globSync('src/*.js');
+const inputPaths = globSync("src/*.js");
 
 export default inputPaths.map((inputPath) => {
-	const inputFilenameWithoutExtension = path.basename(inputPath, '.js');
+	const inputFilenameWithoutExtension = path.basename(inputPath, ".js");
 	return {
-    input: [inputPath],
-    external: ['jquery'],
-    output: [
-      {
-        file: `dist/umd/${inputFilenameWithoutExtension}.min.js`,
-        format: 'umd',
-				name: 'jCanvas',
-        sourcemap: true,
+		input: [inputPath],
+		external: ["jquery"],
+		output: [
+			{
+				file: `dist/umd/${inputFilenameWithoutExtension}.min.js`,
+				format: "umd",
+				name: "jCanvas",
+				sourcemap: true,
 				globals: {
-					jquery: '$'
-				}
-      },
-      {
-        file: `dist/esm/${inputFilenameWithoutExtension}.min.js`,
-        format: 'esm',
-        sourcemap: true,
+					jquery: "$",
+				},
+			},
+			{
+				file: `dist/esm/${inputFilenameWithoutExtension}.min.js`,
+				format: "esm",
+				sourcemap: true,
 				globals: {
-					jquery: '$'
-				}
-      }
-    ],
-    plugins: [commonjs(), terser()]
-  };
+					jquery: "$",
+				},
+			},
+		],
+		plugins: [commonjs(), terser()],
+	};
 });
