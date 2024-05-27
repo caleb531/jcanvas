ed8a766a0345e64c451213b56b0ba2d555af946f
Caleb Evans <caleb@calebevans.me>
2024-05-27 14:53:10 -0700

    Reformat Rollup config to use 2-space indent

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,29 +6,29 @@ import esbuild from "rollup-plugin-esbuild";
 const inputPaths = globSync(["src/jcanvas.ts", "src/jcanvas-*.ts"]);
 
 export default inputPaths.map((inputPath) => {
-	const inputFilenameWithoutExtension = path.basename(inputPath, ".ts");
-	return {
-		input: [inputPath],
-		external: ["jquery", "jcanvas"],
-		output: [
-			{
-				file: `dist/umd/${inputFilenameWithoutExtension}.min.js`,
-				format: "umd",
-				name: `jCanvas_${inputFilenameWithoutExtension}`,
-				sourcemap: true,
-				globals: {
-					jquery: "$",
-				},
-			},
-			{
-				file: `dist/esm/${inputFilenameWithoutExtension}.min.js`,
-				format: "esm",
-				sourcemap: true,
-				globals: {
-					jquery: "$",
-				},
-			},
-		],
-		plugins: [commonjs(), esbuild({ minify: true })],
-	};
+  const inputFilenameWithoutExtension = path.basename(inputPath, ".ts");
+  return {
+    input: [inputPath],
+    external: ["jquery", "jcanvas"],
+    output: [
+      {
+        file: `dist/umd/${inputFilenameWithoutExtension}.min.js`,
+        format: "umd",
+        name: `jCanvas_${inputFilenameWithoutExtension}`,
+        sourcemap: true,
+        globals: {
+          jquery: "$",
+        },
+      },
+      {
+        file: `dist/esm/${inputFilenameWithoutExtension}.min.js`,
+        format: "esm",
+        sourcemap: true,
+        globals: {
+          jquery: "$",
+        },
+      },
+    ],
+    plugins: [commonjs(), esbuild({ minify: true })],
+  };
 });
