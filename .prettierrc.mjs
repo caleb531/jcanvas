e65047557601dcee54e223cf23d4a6b8201bede8
Caleb Evans <caleb@calebevans.me>
2024-05-30 13:53:15 -0700

    Add Prettier to pipeline
    Even though it was already applying to every file on save in VS Code.

---
+++ .prettierrc.mjs
@@ -0,0 +1,7 @@
+export default {
+	// Use double quotes almost always
+	singleQuote: false,
+	// Trailing commas where valid in ES5 (objects, arrays, etc.). Trailing
+	// commas in type parameters in TypeScript and Flow.
+	trailingComma: 'es5'
+};
