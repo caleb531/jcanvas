411cb8be3bde404fd237116eba10e7595175db1a
Caleb Evans <caleb@calebevans.me>
2024-05-30 16:12:37 -0700

    Use double quotes in Prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,5 +3,5 @@ export default {
 	singleQuote: false,
 	// Trailing commas where valid in ES5 (objects, arrays, etc.). Trailing
 	// commas in type parameters in TypeScript and Flow.
-	trailingComma: 'es5'
+	trailingComma: "es5",
 };
