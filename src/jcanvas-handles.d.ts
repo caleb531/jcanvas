590c332aecc0f17eb4eeeddf09ba382d162cc49d
Caleb Evans <caleb@calebevans.me>
2024-06-14 11:38:35 -0700

    Make JCanvasLayerWithHandles compatible with extended JCanvasLayer

--- src/jcanvas-handles.d.ts
+++ src/jcanvas-handles.d.ts
@@ -1,14 +1,14 @@
 /// <reference path="jcanvas.d.ts" />
 
 interface JCanvasDefaults {
-	handle: JCanvasLayer | null;
-	guide: JCanvasLayer | null;
-	aspectRatio: number | null;
-	resizeFromCenter: boolean;
-	constrainProportions: boolean;
-	handlePlacement: "corners" | "sides" | "both";
-	minWidth: number;
-	minHeight: number;
+	handle?: JCanvasLayer | null;
+	guide?: JCanvasLayer | null;
+	aspectRatio?: number | null;
+	resizeFromCenter?: boolean;
+	constrainProportions?: boolean;
+	handlePlacement?: "corners" | "sides" | "both";
+	minWidth?: number;
+	minHeight?: number;
 	handlestart?: JCanvasLayerCallback;
 	handlestop?: JCanvasLayerCallback;
 	handlemove?: JCanvasLayerCallback;
